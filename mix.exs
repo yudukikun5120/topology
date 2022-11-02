a4b67cf07b6bef0a1abfd678bb2ef535a606604d
Yuzuki Arai <yudukikun5120@gmail.com>
2022-11-03 00:57:13 +0900

    build: 0.1.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Topology.MixProject do
     [
       app: :topology,
       description: "Returns the topology of a set.",
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
