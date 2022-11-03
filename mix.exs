a3494752dc4338ae7600e54febb54e8c4b5d6240
Yuzuki Arai <yudukikun5120@gmail.com>
2022-11-03 17:29:59 +0900

    chore: add :droplet: ExCoveralls to deps

--- mix.exs
+++ mix.exs
@@ -14,6 +14,13 @@ defmodule Topology.MixProject do
         licenses: ["MIT"],
         links: %{"GitHub" => "https://github.com/yudukikun5120/topology"},
         source_url: "https://github.com/yudukikun5120/topology"
+      ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
       ]
     ]
   end
@@ -29,7 +36,8 @@ defmodule Topology.MixProject do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.1", only: [:dev]}
+      {:dialyxir, "~> 1.1", only: [:dev]},
+      {:excoveralls, "~> 0.10", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
